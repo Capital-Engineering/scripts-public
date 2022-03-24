d5b19389c093ffd393ec0b0c7e0cb15bcf9aa470
mreid-capital <102324790+mreid-capital@users.noreply.github.com>
2022-03-24 16:03:04 -0700

    Update configure_remove_apps-PRO1.ps1

--- configure_remove_apps-PRO1.ps1
+++ configure_remove_apps-PRO1.ps1
@@ -1,31 +1,3 @@
-<#
-.SYNOPSIS
-    Remove built-in apps (modern apps) from Windows 10.
-
-.DESCRIPTION
-    This script will remove all built-in apps with a provisioning package that's not specified in the 'white-list' in this script.
-    It supports MDT and ConfigMgr usage, but only for online scenarios, meaning it can't be executed during the WinPE phase.
-
-    For a more detailed list of applications available in each version of Windows 10, refer to the documentation here:
-    https://docs.microsoft.com/en-us/windows/application-management/apps-in-windows-10
-
-.EXAMPLE
-    .\Invoke-RemoveBuiltinApps.ps1
-
-.NOTES
-    FileName:    Invoke-RemoveBuiltinApps.ps1
-    Author:      Nickolaj Andersen
-    Contact:     @NickolajA
-    Created:     2019-03-10
-    Updated:     2021-02-02
-
-    Version history:
-    1.0.0 - (2019-03-10) Initial script updated with help section and a fix for randomly freezing
-    1.1.0 - (2019-05-03) Added support for Windows 10 version 1903 (19H1)
-    1.1.1 - (2019-08-13) Removed the part where it was disabling/enabling configuration for Store updates, as it's not needed
-    1.1.2 - (2019-10-03) Removed unnecessary left over functions and updated catch statements so that they actually log the current app that could not be removed
-    1.2.0 - (2021-02-02) Added support for Windows 10 version 2004 (20H1) and 20H2
-#>
 Begin {
     # White list of Features On Demand V2 packages
     $WhiteListOnDemand = "NetFX3|DirectX|Tools.DeveloperMode.Core|Language|InternetExplorer|ContactSupport|OneCoreUAP|WindowsMediaPlayer|Hello.Face|Notepad|MSPaint|PowerShell.ISE|ShellComponents"
